3e35ed5d53c87c0cf35b256d12e4fb804a00a4db
mina1112 <mina.kato8885@gmail.com>
2021-05-23 11:01:05 +0900

    ＋ボタン

---
+++ app/src/main/java/app/kato/nim/count_app/MainActivity.kt
@@ -0,0 +1,24 @@
+package app.kato.nim.count_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var tapCount = 0
+
+        tapButton.setOnClickListener {
+            tapCount = tapCount ++
+            countText.text = tapCount.toString()
+        }
+
+       /* if (tapCount % 3 == 0) {
+            countText.setTextColor( 90, 255, 25)
+        }
+
+    }*/
+}
